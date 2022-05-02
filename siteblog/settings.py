7791beb4d90bbbaa923dee476380f3baf54506bc
Serg <shop@smit.ua>
2022-05-02 17:53:58 +0300

    base dir

--- siteblog/settings.py
+++ siteblog/settings.py
@@ -13,7 +13,8 @@ import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -128,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'siteblog/static/'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
